6787db1d3591662cea8099e048ac93a600d9108a
Klapeyron <Nikitoslav-razdol@ya.ru>
2017-01-26 16:04:38 +0300

    Check different summation.
    From 0 to N-1 compare to
    from 1 to N

--- app/src/main/java/space/klapeyron/mtracker/DFT.java
+++ app/src/main/java/space/klapeyron/mtracker/DFT.java
@@ -9,8 +9,11 @@ import android.util.Log;
 public class DFT {
     private short[] xn;
     private double[] Xk;
-    double[] maxXk = {-1,-1,-1};
-    int[] maxk = {-1,-1,-1};
+    private double[] Xk2;
+    double[] maxXk = {-1,-1,-1,-1,-1};
+    int[] maxk = {-1,-1,-1,-1,-1};
+    double[] maxXk2 = {-1,-1,-1,-1,-1};
+    int[] maxk2 = {-1,-1,-1,-1,-1};
     public static double highk;
 
     public DFT() {
@@ -42,12 +45,17 @@ public class DFT {
         //    Log.i("TAG","DFT: "+this);
             int N = xn.length;
             DFT.highk = 2000.0/44100.0*N;
-            Log.i("TAG","HIGHK " + highk);
+        //    Log.i("TAG","HIGHK " + highk);
             Xk = new double[N/2];
-            maxXk = new double[] {0,0,0};
-            maxk = new int[] {0,0,0};
+            Xk2 = new double[N/2];
+            maxXk = new double[] {0,0,0,0,0};
+            maxk = new int[] {0,0,0,0,0};
+            maxXk2 = new double[] {0,0,0,0,0};
+            maxk2 = new int[] {0,0,0,0,0};
             double re = 0;
             double im = 0;
+            double re2 = 0;
+            double im2 = 0;
             float pi2N = 2*(float)3.1415926535/(float) N;
             double xnwn = 0;
 
@@ -56,30 +64,63 @@ public class DFT {
                     xnwn = xn[i]*0.5*(1-Math.cos(2*(float)3.1415926535*i/(N-1)));
                     re += xnwn*Math.cos(pi2N*j*i);
                     im += xnwn*Math.sin(pi2N*j*i);
+                    xnwn = xn[i]*0.5*(1-Math.cos(2*(float)3.1415926535*i/(N-1)));
+                    re2 += xnwn*Math.cos(pi2N*j*(i+1));
+                    im2 += xnwn*Math.sin(pi2N*j*(i+1));
                 }
                 Xk[j] += Math.pow(Math.pow(re,2)+Math.pow(im,2),0.5);
+                Xk2[j] += Math.pow(Math.pow(re,2)+Math.pow(im,2),0.5);
+                if(Xk[j]!=Xk2[j])
+                    Log.i("TAG","false");
 
-            /*    for(int p=0;p<3;p++) {
+            /*    for(int p=0;p<5;p++) {
                     if (Xk[j]>maxXk[p]) {
-                        for(int l=2;l>p;l--) {
+                        for(int l=4;l>p;l--) {
                             maxXk[l]=maxXk[l-1];
                         }
                         maxXk[p] = Xk[j];
                         maxk[p] = j;
-                        p = 3;
+                        p = 5;
+                    }
+                }
+
+                for(int p=0;p<5;p++) {
+                    if (Xk2[j]>maxXk2[p]) {
+                        for(int l=4;l>p;l--) {
+                            maxXk2[l]=maxXk2[l-1];
+                        }
+                        maxXk2[p] = Xk2[j];
+                        maxk2[p] = j;
+                        p = 5;
                     }
                 }*/
 
-                if (Xk[j]>maxXk[0]) {
+            /*    if (Xk[j]>maxXk[0]) {
                     maxXk[0] = Xk[j];
                     maxk[0] = j;
-                }
+                }*/
             }
 
-            float helpN = (float)44100/(float)N;
+        /*    float helpN = (float)44100/(float)N;
             Log.i("TAG","DFT: "+this+" max freq: " + (float)maxk[0]*helpN+" " + (float)maxk[1]*helpN
                     +" " + (float)maxk[2]*helpN);
+            Log.i("TAG","DFT: "+this+" max freq: " + (float)maxk2[0]*helpN+" " + (float)maxk2[1]*helpN
+                    +" " + (float)maxk2[2]*helpN);
+            if (!isArraysEqual(maxk,maxk2))
+                Log.i("TAG","EQUAL false");*/
             this.interrupt();
         }
+    }
+
+    private Boolean isArraysEqual(int[] a1, int[] a2) {
+        Boolean f = true;
+        if (a1.length == a2.length) {
+            for (int i=0;i<a1.length;i++) {
+                if(a1[i] != a2[i]) {
+                    f = false;
+                }
+            }
+        }
+        return f;
     }
 }
